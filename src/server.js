614f125f6f17e262b52d6ddda1160dcd83f5c38e
awaghat99 <awaghat99@gmail.com>
2023-09-28 17:21:46 +0100

    add cors to the server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 const userRouter = require("./routes/routes");
 const User = require("./models/users");
 
 const port = process.env.PORT || 5001;
+
 const syncTables = () => {
-  User.sync();
+  User.sync({ alter: true });
   // creates the user table if it does not already exist
 };
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 
